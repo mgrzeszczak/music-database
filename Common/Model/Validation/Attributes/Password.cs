c53a1f4b65fa7e92330f77600b866517ccb64fba
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2016-12-06 00:24:14 +0100

    all files.

---
+++ Common/Model/Validation/Attributes/Password.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Model.Validation.Validators;
+using NHibernate.Validator.Engine;
+
+namespace Common.Model.Validation.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    [ValidatorClass(typeof(PasswordValidator))]
+    class Password : Attribute,IRuleArgs
+    {
+        private string message = "Password must have at least 1 upper- and lowercase character, at least 1 digit, at least 1 special character and minimum 8 characters";
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+    }
+}
---
+++ Desktop/Data/RestResponseExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Exception;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Desktop.Data
+{
+    public static class RestResponseExtensions
+    {
+        private static Newtonsoft.Json.JsonSerializer serializer = new JsonSerializer();
+
+        public static bool Succeeded(this IRestResponse resp)
+        {
+            return resp.StatusCode == HttpStatusCode.OK
+                   || resp.StatusCode == HttpStatusCode.Created
+                   || resp.StatusCode == HttpStatusCode.NoContent
+                   || resp.StatusCode == HttpStatusCode.Accepted;
+        }
+
+        public static ExceptionResponse ExceptionResponse(this IRestResponse resp)
+        {
+            if (resp.Succeeded()) return null;
+            using (var strReader = new StringReader(resp.Content))
+            {
+                using (var jsonReader = new JsonTextReader(strReader))
+                {
+                    return serializer.Deserialize<ExceptionResponse>(jsonReader);
+                }
+            }
+        }
+    }
+}
