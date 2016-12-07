3dd9fc08f8ee534e86e1ba110ffa8ecc47cda49e
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2016-12-07 18:45:27 +0100

    Length format changes to x min x s.

--- Desktop/Converters/TimeSpanConverter.cs
+++ Desktop/Converters/TimeSpanConverter.cs
@@ -13,9 +13,9 @@ namespace Desktop.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             TimeSpan length = (TimeSpan)value;
-            //return length.Hours*60 + length.Minutes + ":" + length.Seconds;
+            return $"{length.Hours*60 + length.Minutes} min {length.Seconds} s";
             //return length.ToString(@"hh\:mm\:ss");
-            return length.ToString(@"h\h\ m\ \m\i\n\ s\s");
+            //return length.ToString(@"h\h\ m\ \m\i\n\ s\s");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
